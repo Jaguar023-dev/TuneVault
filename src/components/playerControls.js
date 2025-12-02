28ffd4b75b4bd6c19a35dbe251b71eb868df4615
Jaguar023-dev <derrickithicia8@gmail.com>
2025-12-02 12:27:12 +0300

    Add PlayerControls component for play/pause functionality

--- src/components/playerControls.js
+++ src/components/playerControls.js
@@ -1,1 +1,22 @@
+// src/components/PlayerControls.js
 
+import React from "react";
+import { View, Button } from "react-native";
+
+export default function PlayerControls({ isPlaying, onPlay, onPause }) {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-around",
+        marginVertical: 20,
+      }}
+    >
+      {isPlaying ? (
+        <Button title="Pause" onPress={onPause} />
+      ) : (
+        <Button title="Play" onPress={onPlay} />
+      )}
+    </View>
+  );
+        }
